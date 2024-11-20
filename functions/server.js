2b7163845eaa349b820b6f7e4c1ab22e23025d79
baekusboo <g.sai.sunitha@gmail.com>
2024-11-20 21:10:54 +0530

    netlify changes

--- functions/server.js
+++ functions/server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 require('dotenv').config();
+const serverless = require('serverless-http');
 
 const app = express();
 app.use(cors());
@@ -44,3 +45,5 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
+
+module.exports.handler = serverless(app);
